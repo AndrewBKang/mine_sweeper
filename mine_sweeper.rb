5f858d3be260bdc39bf3eb8e0ce07fcced43eaa5
Jin Park and Andrew Kang <Jin+Andrew@appacademy.io>
2013-06-24 10:26:44 -0400

    first commit

---
+++ mine_sweeper.rb
@@ -0,0 +1,39 @@
+class Board
+  def initialize()
+    #choose board
+  end
+  def set_bombs
+    #randomly choose bombs
+  end
+  def set_fringes
+    #finds fringes from bomb placements
+  end
+  def set_empty
+    #everything else is empty
+  end
+  def show_board
+  end
+end
+
+class Minesweeper
+  def play
+  end
+  def run
+  end
+  def win?
+  end
+end
+
+class Player
+  def initialize
+
+  end
+
+  def reveal
+
+  end
+
+  def flag
+  end
+
+end
